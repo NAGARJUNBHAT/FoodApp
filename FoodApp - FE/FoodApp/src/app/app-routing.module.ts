bf54d2cc6cbc0c63dd1690f0f57e15664557a5b8
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-09-29 12:54:33 +0530

    added routing and navbar

--- FoodApp - FE/FoodApp/src/app/app-routing.module.ts
+++ FoodApp - FE/FoodApp/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
