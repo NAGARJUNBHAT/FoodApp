0fcaba9503321d20f1c08930fda92acb45251b60
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-09-29 15:46:03 +0530

    Added order viewing page

---
+++ FoodApp - FE/FoodApp/src/app/staff/staff.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff',
+  templateUrl: './staff.component.html',
+  styleUrls: ['./staff.component.css'],
+})
+export class StaffComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  allOrders = [{}];
+}
