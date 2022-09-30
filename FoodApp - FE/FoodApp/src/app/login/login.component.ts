d5133dfd4cca3d48be0ed0a17b1097e83397f957
harshada <harshada.ashokkanade@clarivate.com>
2022-09-30 10:15:17 +0530

    login update

--- FoodApp - FE/FoodApp/src/app/login/login.component.ts
+++ FoodApp - FE/FoodApp/src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
     if(this.res.error){
       console.log("Invalid credentials");
       alert(this.res.message);
-    }else if(this.res.data.role === "manager"){
+    }else if(this.res.data.role === "BranchManager"){
       localStorage.setItem("user", JSON.stringify(this.res.data))
       alert(this.res.message);
       this.route.navigate(["/manager"])
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
       localStorage.setItem("user", JSON.stringify(this.res.data))
       alert(this.res.message);
       this.route.navigate(["/staff"])
-    }else if(this.res.data.role === "User"){
+    }else if(this.res.data.role === "user"){
       alert(this.res.message);
       this.route.navigate(["/user-dashboard"])
     }
