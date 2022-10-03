e6fe4fa3452ae03c763da467aaf8a19da4300461
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-10-03 10:54:46 +0530

    create order component updated

--- FoodApp - FE/FoodApp/src/app/create-order/create-order.component.ts
+++ FoodApp - FE/FoodApp/src/app/create-order/create-order.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { FoodorderService } from './../Services/foodorder.service';
 import { FoodproductService } from './../Services/foodproduct.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-order.component.css'],
 })
 export class CreateOrderComponent implements OnInit {
-  constructor(private fPservice: FoodproductService) {}
+  res: any;
+  staff = JSON.parse(localStorage.getItem('user')!);
+  itemCountMap = new Map<number, number>();
+  orderItems: any;
+  totalOrderPrice: number = 0;
+
+  constructor(
+    private fPservice: FoodproductService,
+    private fOService: FoodorderService,
+    private route: Router
+  ) {}
 
   allFoodProducts: any;
   ngOnInit(): void {
@@ -17,5 +30,57 @@ export class CreateOrderComponent implements OnInit {
       this.allFoodProducts = this.allFoodProducts.data;
       console.log('First Food Product : ', this.allFoodProducts[0]);
     });
+  }
+
+  addNewItem(itemId: any, itemPrice: any) {
+    // console.log(itemId, itemPrice);
+    // If item has been added previously (just the quantity is changed)
+    if (this.itemCountMap.has(itemId)) {
+      // Increase count and totalPrice
+      this.itemCountMap.set(itemId, this.itemCountMap.get(itemId)! + 1);
+      this.totalOrderPrice += itemPrice;
+    }
+    // If chosen for the first time
+    else {
+      // Set count and increase orderPrice
+      this.itemCountMap.set(itemId, 1);
+      this.totalOrderPrice += itemPrice;
+    }
+    // console.log(this.totalOrderPrice);
+  }
+
+  removeItem(itemId: any, itemPrice: any) {
+    // console.log(itemId, itemPrice);
+    if (this.itemCountMap.has(itemId)) {
+      if (this.itemCountMap.get(itemId) === 0) {
+      } else {
+        this.itemCountMap.set(itemId, this.itemCountMap.get(itemId)! - 1);
+        this.totalOrderPrice -= itemPrice;
+      }
+    }
+    // console.log(this.totalOrderPrice);
+  }
+  reply: any;
+  addNewOrder(form: NgForm) {
+    let newOrder = {
+      status: true,
+      customerName: form.value.customerName,
+      customerContact: form.value.contactNumber,
+      totalPrice: this.totalOrderPrice,
+      orderCreatedTime: new Date().toString(),
+    };
+    console.log('Staff id: ' + this.staff.id);
+    console.log('New Order Made : ', newOrder);
+    this.reply = confirm('Do you want to place the order? ');
+    if (this.reply == true) {
+      this.fOService.saveFoodOrder(this.staff.id, newOrder).subscribe((r) => {
+        this.res = r;
+        console.log(this.res.message);
+        if (!this.res.error) {
+          alert('Food Order made successfully!');
+          this.route.navigate(['/staff']);
+        }
+      });
+    }
   }
 }
